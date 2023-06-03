b6d53f8747ad86adc562df2ecdfad2f5f6760b82
Kacper Plusa <plusakacper@gmail.com>
2023-06-03 16:12:36 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# IT Conference
+
+Website project for IT conference support.
---
+++ src/test/java/com/kacperp/itconference/ItConferenceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kacperp.itconference;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItConferenceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
