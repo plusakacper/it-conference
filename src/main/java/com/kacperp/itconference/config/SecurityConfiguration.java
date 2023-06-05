aaedb39ddfb971e2eb4c285d611a0ea61c6b9eca
plusakacper <plusakacper@gmail.com>
2023-06-05 19:57:49 +0200

    add h2 config

---
+++ src/main/java/com/kacperp/itconference/config/SecurityConfiguration.java
@@ -0,0 +1,18 @@
+package com.kacperp.itconference.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+	@Bean
+	public WebSecurityCustomizer webSecurityCustomizer() {
+		return (web) -> web.ignoring().requestMatchers(new AntPathRequestMatcher("/h2-console/**"));
+	}
+
+}
